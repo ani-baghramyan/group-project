296f80e95a8f452f56b2cc4529c2440998180445
ArevikShahinyan1 <arevikshahinyan2006@gmail.com>
2025-04-12 20:19:20 +0400

    Update Planet.java
    Overridden toString() method in Planet class to provide formatted object output

--- Planet.java
+++ Planet.java
@@ -3,7 +3,7 @@ public class Planet extends CelestialBody{
   private int numberOfMoons;
   private String surfaceConditions;
 
-  public Planet(String name, double distanceFromEarth, double mass, double temperature, 
+  public Planet(String name, double distanceFromEarth, double mass, double temperature, String type,
                 String atmosphericComposition, int numberOfMoons, String surfaceConditions){
     super(name, distanceFromEarth, mass, temperature);
     this.atmosphericComposition = atmosphericComposition;
@@ -20,13 +20,24 @@ public class Planet extends CelestialBody{
   
   public String getAtmosphericComposition() {
         return atmosphericComposition;
-    }
+  }
   
   public int getNumberOfMoons() {
         return numberOfMoons;
-    }
+  }
 
   public String getSurfaceConditions() {
         return surfaceConditions;
-    }
+  }
+  
+  public String toString() {
+    return "Planet Name: " + getName() + "\n" +
+           "Type: " + getType() + "\n" +
+           "Distance from Earth: " + getDistanceFromEarth() + " light years\n" +
+           "Mass: " + getMass() + " kg\n" +
+           "Temperature: " + getTemperature() + " K\n" +
+           "Atmosphere: " + atmosphericComposition + "\n" +
+           "Moons: " + numberOfMoons + "\n" +
+           "Surface: " + surfaceConditions + "\n";
+  }
 }

ae29e5415213aac7515813f7f85ef356fc5283e6
ArevikShahinyan1 <arevikshahinyan2006@gmail.com>
2025-04-13 22:31:42 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,8 +6,11 @@ public class Main {
         Star sun = new Star("Sun", 0.0000158, "Yellow dwarf", 5778, 1, "white", 1, 696.340);
         Star proxima = new Star("Proxima Centauri", 4.2, "Red Dwarf", 3042, 12, "red", 0.17, 14);
 
-        Planet earth = new Planet("Earth", 0.0, true, 6371);
-        Planet mars = new Planet("Mars", 0.0, false, 3389);
+        Planet earth = new Planet("Earth", 0.0, 5.972e24, 288, "Terrestrial","Nitrogen (78%) and Oxygen (21%)",
+                                  1, "Solid, stable and active surface with mountains, valleys, canyons, plains and more.", 6.378e6);
+        
+        Planet mars = new Planet("Mars", 2.3815e-11, 6.39e23, 210, "Terrestrial", "Carbon Dioxide (95%), Nitrogen (2.8%)", 
+                                 2,  "Rocky surface with canyons and dust storms", 3.3895e6);
 
         System.out.println("Enter the name of the galaxy:");
         String galaxyName = scanner.nextLine();

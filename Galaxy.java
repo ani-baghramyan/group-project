91977c0555518993fcb23dd1998a83ba76030c64
ani-baghramyan <anibaghramyan06@gmail.com>
2025-04-29 14:27:30 +0400

    Update Galaxy.java
    ArrayList is added to all methods

--- Galaxy.java
+++ Galaxy.java
@@ -1,14 +1,16 @@
-public class Galaxy extends CelestialBody {
+import java.util.ArrayList;
+
+public class Galaxy extends SpaceObject {
     private long numberOfStars;
     private double diameter;
     private int age;
 
-    private Star[] stars;
-    private Planet[] planets;
+    private ArrayList<Star> stars;
+    private ArrayList<Planet> planets;
 
     public Galaxy(String name, double distanceFromEarth, double mass, double temperature, String type,
                   long numberOfStars, double diameter, int age,
-                  Star[] stars, Planet[] planets) {
+                  ArrayList<Star> stars, ArrayList<Planet> planets) {
         super(name, distanceFromEarth, mass, temperature, type);
         this.numberOfStars = numberOfStars;
         this.diameter = diameter;
@@ -17,49 +19,62 @@ public class Galaxy extends CelestialBody {
         this.planets = planets;
     }
 
-    public void displayInfo() {
-        System.out.println("\n--- Galaxy Information ---");
-        System.out.println("Galaxy Name: " + getName());
-        System.out.println("Type: " + getType());
-        System.out.println("Age: " + age + " billion years");
-        System.out.println("Diameter: " + diameter + " light years");
-        System.out.println("Distance from Earth: " + getDistanceFromEarth() + " million light years");
-        System.out.println("Estimated Number of Stars: " + numberOfStars);
-
-        System.out.println("\nStars in this galaxy:");
-        if (stars.length == 0) {
-            System.out.println(" - No stars listed.");
-        } else {
-            for (Star star : stars) {
-                System.out.println(" - " + star.getName());
-            }
-        }
-
-        System.out.println("\nPlanets in this galaxy:");
-        if (planets.length == 0) {
-            System.out.println(" - No planets listed.");
-        } else {
-            for (Planet planet : planets) {
-                System.out.println(" - " + planet.getName());
-            }
-        }
+    public long getNumberOfStars() {
+        return numberOfStars;
     }
 
-    public Star findStarByName(String starName) {
+    public double getDiameter() {
+        return diameter;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public ArrayList<Star> getStars() {
+        return stars;
+    }
+
+    public ArrayList<Planet> getPlanets() {
+        return planets;
+    }
+
+    public Star findStarByName(String name) {
         for (Star star : stars) {
-            if (star.getName().equalsIgnoreCase(starName)) {
+            if (star.getName().equalsIgnoreCase(name)) {
                 return star;
             }
         }
         return null;
     }
 
-    public Planet findPlanetByName(String planetName) {
+    public Planet findPlanetByName(String name) {
         for (Planet planet : planets) {
-            if (planet.getName().equalsIgnoreCase(planetName)) {
+            if (planet.getName().equalsIgnoreCase(name)) {
                 return planet;
             }
         }
         return null;
+    }
+
+    public void displayInfo() {
+        System.out.println("\nGalaxy: " + getName());
+        System.out.println("Type: " + getType());
+        System.out.println("Distance from Earth: " + getDistanceFromEarth() + " light-years");
+        System.out.println("Mass: " + getMass() + " kg");
+        System.out.println("Temperature: " + getTemperature() + " K");
+        System.out.println("Diameter: " + diameter + " light-years");
+        System.out.println("Number of stars: " + numberOfStars);
+        System.out.println("Age: " + age + " billion years");
+
+        System.out.println("\nStars:");
+        for (Star star : stars) {
+            System.out.println("- " + star.getName());
+        }
+
+        System.out.println("\nPlanets:");
+        for (Planet planet : planets) {
+            System.out.println("- " + planet.getName());
+        }
     }
 }

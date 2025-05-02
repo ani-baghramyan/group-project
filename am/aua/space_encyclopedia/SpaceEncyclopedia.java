20029c40795ad312b6f08ada4edc7559b11c1ea0
ArevikShahinyan1 <arevikshahinyan2006@gmail.com>
2025-05-02 17:48:11 +0400

    Update SpaceEncyclopedia.java

--- am/aua/space_encyclopedia/SpaceEncyclopedia.java
+++ am/aua/space_encyclopedia/SpaceEncyclopedia.java
@@ -1,6 +1,6 @@
 package am.aua.space_encyclopedia;
 import java.io.*;
-import am.aua.space_encyclopedia.core;
+import am.aua.space_encyclopedia.core.*;
 
 public class SpaceEncyclopedia {
    private  DataManager dataManager;
